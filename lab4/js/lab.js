c71fb3bfbc0fa5022b4f892227f6c16d90e21922
malumend <maya19173@gmail.com>
2021-10-13 17:09:59 -0700

    lab 4
    lab 4 pseudocoding

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+// Summary.
+// Our pseudocode for lab 4 is a coin flip simulation for a user to test odds
+//against.
+// @link.   URL
+// @file.  This files defines the MyClass class.
+// Author: Izzy Lasarow.
+// Date:  10/13/21.
+
+// Ask user to select heads or tails
+  //Get user input
+  // Validate user input
+  // Test of win or loose condtion
