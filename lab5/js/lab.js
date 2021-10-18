02a2a2df0b15727d6cb64a82ea49219dce2ebf74
malumend <91556513+malumend@users.noreply.github.com>
2021-10-18 16:26:32 -0700

    LAB 5
    DATA TYPES AND VARIABLES

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/**
+  *Author:Maya Mendoza
+  *Created:10.18.2021
+  *
+  **/
+  //Define Variables
+  make:"Nissan"
+  model:"Altima"
+  color:"Red"
+  year:"2008"
+
+  //Calculate
+  age= 2021-year;
+
+  //output
+  document.writeln("Make:"+ make + "<br>");
+  document.writeln("Model:"+ model + "<br>");
+  document.writeln("Color:"+ color + "<br>");
+  document.writeln("Year:"+ year + "<br>");
+  document.writeln("Age:"+ age + "years<br>");
