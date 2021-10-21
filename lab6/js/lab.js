4333678daf149a5c00a4a03eab75be07874c7f76
malumend <91556513+malumend@users.noreply.github.com>
2021-10-20 23:28:55 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,19 +1,19 @@
-/*
+/**
   * Author: Maya Mendoza <malumend@ucsc.edu>
   * Created: 20 October 2021
   * License: Public Domain
-*/
+**/
 
 // Define Variables
-myTransport = ["car" , "bus", "walking", "rides from friends" , "bike"]
+myTransport = ["car" , "bus", "walking", "rides from friends" , "bike"];
 
 myMainRide = {
-      make = "Nissan";
-      model = "Altima";
-      color = "Rodeo Red";
-      year = 2008;
-      age = function() {
-        return 2021 - age;
+      make : "Nissan",
+      model : "Altima",
+      color : "Red",
+      year : 2008,
+      age : function() {
+        return 2021 - year;
       }
 }
 
